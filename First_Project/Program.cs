e7c000fc531782a2df512a1e57aa06245654e639
User <User@DESKTOP-JU8V0TU>
2024-11-06 10:13:37 +0200

    Add project files.

---
+++ First_Project/Program.cs
@@ -0,0 +1,12 @@
+﻿int totalsquares = 64;
+long totalgrains = 0;
+
+for (int square = 0; square <= totalsquares; square++)
+{
+    long grainsonsquare = (long)Math.Pow(2, square -1);
+    totalgrains += grainsonsquare;
+
+    Console.WriteLine($"Square {square}: {grainsonsquare} grain(s)");
+
+}
+Console.WriteLine($"\nTotal grains on the chessboard: {totalgrains}");
