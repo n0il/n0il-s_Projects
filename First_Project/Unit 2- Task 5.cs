5318575045ae1363f98661445785d7ba28f982b3
n0il <simeonyordanov2005@gmail.com>
2024-11-21 10:59:40 +0200

    Add files via upload

---
+++ First_Project/Unit 2- Task 5.cs
@@ -0,0 +1,29 @@
+﻿/* # Instructions
+
+Reverse a string
+
+For example:
+input: "cool"
+output: "looc" */
+using System;
+
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a string:");
+        string input = Console.ReadLine();
+        string reversed = ReverseString(input);
+        Console.WriteLine("Reversed string: " + reversed);
+    }   
+
+
+
+    public static string ReverseString(string s)
+    { 
+        char[] cArray = s.ToCharArray();
+        Array.Reverse(cArray);
+        return new string(cArray);
+    }
+}
